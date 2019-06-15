91226c940993dd3950c46ade3a47a827b95f4ca6
pedrituh9 <pedro9cruz@gmail.com>
2019-06-15 16:59:41 +0100

    grid added

---
+++ src/org/academiadecodigo/bootcamp/codebytes/Grid/Grid.java
@@ -0,0 +1,64 @@
+package org.academiadecodigo.bootcamp.codebytes.Grid;
+
+import org.academiadecodigo.simplegraphics.graphics.Color;
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+
+public class Grid {
+
+    public static final int PADDING = 10;
+    private int cols;
+    private int rows;
+    private int cellSize;
+    private Rectangle rect;
+
+    public Grid() {
+        this.cols = 24;
+        this.rows = 14;
+        cellSize = 50;
+    }
+
+    public void initialization () {
+        rect= new Rectangle(PADDING, PADDING, cols*cellSize, rows*cellSize);
+        rect.setColor(Color.WHITE);
+        rect.draw();
+
+    }
+
+    public int getCols() {
+        return cols;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getWidth() {
+        return rect.getWidth();
+    }
+
+    public int getHeight() {
+        return rect.getHeight();
+    }
+
+    public int getX() {
+        return rect.getX();
+    }
+
+    public int getY() {
+        return rect.getY();
+    }
+
+    public int getCellSize() {
+        return cellSize;
+    }
+
+    public int rowToY(int row) {
+        return (row*cellSize) + PADDING;
+    }
+
+    public int columnToX(int column) {
+        return (column*cellSize) + PADDING;
+    }
+
+
+}
